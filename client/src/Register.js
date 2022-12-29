603ddac87b30b4d3c3116b1d3cbac2f1d1ee16eb
FaizanPotrick <faizan.potrick02@gmail.com>
2022-12-29 17:37:29 +0530

    Update Login and Register

--- client/src/Register.js
+++ client/src/Register.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
   const navigate = useNavigate();
+
   const [user, setUser] = useState({
     email_address: "",
     password: "",
@@ -12,32 +13,34 @@ function Register() {
     isShow: false,
     message: "",
   });
+
   const onChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
+
   const onSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     setIsAlert({
       isShow: false,
       message: "",
     });
-    setIsLoading(true);
-    const res = await fetch("/api/user/registration", {
+    const response = await fetch("/api/user/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(user),
     });
-    const data = await res.json();
+    const data = await response.json();
     setIsLoading(false);
-    if (res.status === 200) {
+    if (response.status === 200) {
       setUser({
         email_address: "",
         password: "",
       });
-      navigate("/post");
+      navigate("/");
       window.location.reload();
     } else {
       setIsAlert({
@@ -46,50 +49,41 @@ function Register() {
       });
     }
   };
+
   return (
     <div className="h-[92vh] flex justify-center items-center">
-      <div className="w-full max-w-sm rounded-lg shadow-md p-8 bg-gray-800 border-gray-700">
+      <div className="w-full max-w-sm rounded-xl shadow-md py-8 px-5 bg-gray-800 border-gray-700">
         <form className="space-y-6" onSubmit={onSubmit}>
-          <h5 className="text-xl font-medium text-white">
-            Register to your account
-          </h5>
+          <div className="text-xl font-medium text-white">
+            Create a new account
+          </div>
           {isAlert.isShow && (
             <div className="text-red-700">{isAlert.message}</div>
           )}
-          <div className="flex flex-col gap-2">
-            <label className="text-sm font-medium text-gray-300">
-              Email Address
-            </label>
-            <input
-              type="email"
-              name="email_address"
-              className="border border-gray-500 text-sm rounded-lg w-full p-2.5 bg-gray-600 text-white"
-              placeholder="Email Address"
-              onChange={onChange}
-              value={user.email_address}
-              maxLength="150"
-              required
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <label className="text-sm font-medium text-gray-300">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              className="border border-gray-500 text-sm rounded-lg w-full p-2.5 bg-gray-600 text-white"
-              placeholder="Password"
-              onChange={onChange}
-              value={user.password}
-              autoComplete="off"
-              maxLength="10"
-              required
-            />
-          </div>
+          <input
+            type="email"
+            name="email_address"
+            className="border border-gray-500 text-sm rounded-lg w-full p-2 bg-gray-600 text-white"
+            placeholder="Email Address"
+            onChange={onChange}
+            value={user.email_address}
+            maxLength="320"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            className="border border-gray-500 text-sm rounded-lg w-full p-2 bg-gray-600 text-white"
+            placeholder="Password"
+            onChange={onChange}
+            value={user.password}
+            autoComplete="off"
+            maxLength="10"
+            required
+          />
           <button
             type="submit"
-            className="w-full text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600"
+            className="w-full text-white font-medium rounded-lg text-sm px-5 py-2 text-center bg-blue-600"
           >
             {isLoading ? "Loading..." : "Register"}
           </button>
